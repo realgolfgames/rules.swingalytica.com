2b275beae359cbca076f32e9c2e0a458af103706
MoinJulian <jh@realgolf.games>
2025-09-26 22:22:26 +0100

    chore: update default language in rules parameters schema from 'en' to 'de'

--- src/lib/schemas/rules_params_schema.ts
+++ src/lib/schemas/rules_params_schema.ts
@@ -4,6 +4,6 @@ export const rules_params_schema = z.object({
   limit: z.coerce.number().int().min(1).max(25).default(25),
   skip: z.coerce.number().int().min(0).max(25).default(0),
   grouped: z.string().optional().default('false'),
-  language: z.string().optional().default('en'),
+  language: z.string().optional().default('de'),
   id: z.coerce.number().int().max(25).optional()
 });
